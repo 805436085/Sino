520e0487623614b53d5cab3fe6ff18f09c301616
spoonzhang <805436085@qq.com>
2023-08-08 00:43:47 +0800

    init

---
+++ Source/SinoByAS/SinoByAS.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SinoByAS.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SinoByAS, "SinoByAS" );
+
+DEFINE_LOG_CATEGORY(LogSinoByAS)
+ 
---
+++ Source/SinoByAS/SinoByASGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SinoByASGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASinoByASGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASinoByASGameMode();
+};
+
+
+
